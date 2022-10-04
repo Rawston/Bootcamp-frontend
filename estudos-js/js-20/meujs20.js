c4ebf1406e5546f07a21f0075963cfc004f9fcc5
Rawston <houston_cam@msn.com>
2022-10-03 23:27:12 -0300

    js-20.js!!!

---
+++ estudos-js/js-20/meujs20.js
@@ -0,0 +1,36 @@
+/* 
+for
+for in
+for of
+forEach
+*/
+
+let listaUm = [9, 22, 54, 14, 21, 08, 59, 01, 40, 36, 13, 27, 84]
+
+let listaDois = ['erro', 'eleva', 'duque', 'damas', 'dados', 'creme',
+    'cofre', 'acode', 'acima', 'achou', 'aches', 'achem'
+]
+
+/* console.log('oi loops');
+
+// for tradicional
+for (var i = 0; i < listaUm.length; i++) {
+    console.log('indice=', i);
+    console.log('indice=', listaUm[i]);
+} */
+
+
+// for in
+/* for (var indice in listaUm) {
+    console.log('indice=', listaUm[indice]);
+}
+ */
+// for of
+/* for (var elemento in listaDois) {
+    console.log('elemento=', listaUm[elemento]);
+} */
+
+// forEach
+listaDois.forEach(function(item) {
+    console.log(item);
+})
